9e79c2fa6ff4cc20c73eec298d2eb4ecb40ac594
Shivam Sharma <136487437+shivamsharmxa@users.noreply.github.com>
2024-12-02 15:22:05 +0530

    Added Navbar and Hero Section

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,16 @@
-
-
 export default function Home() {
   return (
     <>
-    <h1 className="text-3xl">HOME</h1>
+      <section className="pink_container">
+        <h1 className="heading">
+          PITCH YOUR STARTUPS, <br /> CONNECT WITH ENTREPRENEURS
+        </h1>
+
+        <p className="sub-heading !max-w-3xl">
+          Submit Ideas, Vote on Pitches, and Get Noticed in Virtual
+          Competitions.
+        </p>
+      </section>
     </>
   );
 }
